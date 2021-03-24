99d14a8a59b61bdfb897f1ecf0d3b7764e677f5d
memer <mimeremim@gmail.com>
2021-03-24 13:45:38 +0100

    Add files via upload

--- maps/main.js
+++ maps/main.js
@@ -37,10 +37,10 @@ var defaultLayers = platform.createDefaultLayers();
 
 //Step 2: initialize a map - this map is centered over Europe
 var map = new H.Map(document.getElementById('map'),
-defaultLayers.vector.normal.map,{
-center: {lat:50, lng:5},
-zoom: 4,
-pixelRatio: window.devicePixelRatio || 1
+  defaultLayers.vector.normal.map,{
+  center: {lat:50, lng:5},
+  zoom: 4,
+  pixelRatio: window.devicePixelRatio || 1
 });
 // add a resize listener to make sure that the map occupies the whole container
 window.addEventListener('resize', () => map.getViewPort().resize());
